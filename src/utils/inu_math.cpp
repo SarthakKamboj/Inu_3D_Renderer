b33581cb46d4554b0ff71e97cd3f3326c81bc1ee
SarthakKamboj <sarthakkamboj22@gmail.com>
2024-04-02 15:47:49 -0500

    fixed issue...missing ending curly braces for a couple of the functions

--- src/utils/inu_math.cpp
+++ src/utils/inu_math.cpp
@@ -39,32 +39,35 @@ vec2 division(const vec2& vect, float scalar) {
         return { vect.x / scalar, vect.y / scalar };
     else
         return { 0, 0 };
+}
 
-    vec3 addition(const vec3 & vect1, const vec3 & vect2) {
-        return { vect1.x + vect2.x, vect1.y + vect2.y, vect1.z + vect2.z };
-    }
+vec3 addition(const vec3 & vect1, const vec3 & vect2) {
+	return { vect1.x + vect2.x, vect1.y + vect2.y, vect1.z + vect2.z };
+}
 
-    vec3 subtraction(const vec3 & vect1, const vec3 & vect2) {
-        return { vect1.x - vect2.x, vect1.y - vect2.y, vect1.z - vect2.z };
-    }
+vec3 subtraction(const vec3 & vect1, const vec3 & vect2) {
+	return { vect1.x - vect2.x, vect1.y - vect2.y, vect1.z - vect2.z };
+}
 
-    vec3 division(const vec3 & vect, float scalar) {
-        if (scalar != 0)
-            return { vect.x / scalar, vect.y / scalar, vect.z / scalar };
-        else
-            return { 0, 0, 0 };
-    }
+vec3 division(const vec3 & vect, float scalar) {
+	if (scalar != 0)
+		return { vect.x / scalar, vect.y / scalar, vect.z / scalar };
+	else
+		return { 0, 0, 0 };
+}
 
-    vec2 normalize(const vec2 & vect) {
-        float mag = magnitude(vect);
-        if (mag != 0)
-            return { vect.x / mag, vect.y / mag };
-        else
-            return { 0, 0 };
+vec2 normalize(const vec2& vect) {
+	float mag = magnitude(vect);
+	if (mag != 0)
+		return { vect.x / mag, vect.y / mag };
+	else
+		return { 0, 0 };
+}
 
-        vec3 normalize(const vec3 & vect) {
-            float mag = magnitude(vect);
-            if (mag != 0)
-                return { vect.x / mag, vect.y / mag, vect.z / mag };
-            else
-                return { 0, 0, 0 };
+vec3 normalize(const vec3& vect) {
+	float mag = magnitude(vect);
+	if (mag != 0)
+		return { vect.x / mag, vect.y / mag, vect.z / mag };
+	else
+		return { 0, 0, 0 };
+}
