ff46bd537714ea3d8d21d9a644e799d2479ca12d
SarthakKamboj <sarthakkamboj22@gmail.com>
2024-04-02 15:52:41 -0500

    changed main.cpp to be easier to run math tests

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,18 @@
 
 #include "windowing/window.h"
 
+#define TEST_MATH 1
+
 extern window_t window;
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+#if TEST_MATH
+  // add your testing functionality here that you were writing in the int main() in test.cpp
+#else
   create_window(hInstance, 400, 300); 
   while (window.running) {
     poll_events();
     swap_buffers();
   }
+#endif
 }
